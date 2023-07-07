4108657f94fcfc504de80b116a5e747225f42ebb
Валерия Русакова <rusakovalera@gmail.com>
2023-07-07 12:03:35 +0300

    Задание №17: Реализовать загрузку информации о тестируемом приложении из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -1,22 +1,25 @@
+import json
+import os.path
 import pytest
 import allure
 from model.group import Group
 from fixture.application import Application
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
-    global fixture
+    global fixture, target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--base_url")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['base_url'])
+    fixture.session.ensure_login(target['username'], target["password"])
     return fixture
 
 
@@ -30,5 +33,5 @@ def stop(request):
     return fixture
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--base_url", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
 
